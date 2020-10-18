8a17eeacd2a8ea2819a0fd0fa133ee88bad8f1c1
Ezequias Lopes <ezequias@hotmail.com.br>
2020-10-18 15:10:26 -0500

    Update calculator.py

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -17,11 +17,11 @@ def mult(valueA, valueB):
 def div(valueA, valueB):
     return valueA / valueB
 
-
-runAgain = 'y'
+# Calculator function
 
 
 def calculator():
+    # Header
     print(
         "\n"
         "···········································································\n"
@@ -36,30 +36,37 @@ def calculator():
         "3. Multiply\n"
         "4. Divide\n"
     )
+    # Error message
+    invalidInput = "\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\nINVALID INPUT\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n"
 
-    selectedOperation = int(input("Selec operation from 1 to 4: "))
+    selectedOperation = input("Selec operation from 1 to 4: ")
 
-    valueA = int(input("Enter the first value: "))
-    valueB = int(input("Enter the second value: "))
+    if selectedOperation.isnumeric and int(selectedOperation) <= 4:
 
-    if selectedOperation == 1:
-        print(valueA, " + ", valueB, " = ", add(valueA, valueB))
-    elif selectedOperation == 2:
-        print(valueA, " - ", valueB, " = ", sub(valueA, valueB))
-    elif selectedOperation == 3:
-        print(valueA, " * ", valueB, " = ", mult(valueA, valueB))
-    elif selectedOperation == 4:
-        print(valueA, " / ", valueB, " = ", div(valueA, valueB))
+        selectedOperation = int(selectedOperation)
+
+        valueA = int(input("Enter the first value: "))
+        valueB = int(input("Enter the second value: "))
+
+        if selectedOperation == 1:
+            print(valueA, " + ", valueB, " = ", add(valueA, valueB))
+        elif selectedOperation == 2:
+            print(valueA, " - ", valueB, " = ", sub(valueA, valueB))
+        elif selectedOperation == 3:
+            print(valueA, " * ", valueB, " = ", mult(valueA, valueB))
+        elif selectedOperation == 4:
+            print(valueA, " / ", valueB, " = ", div(valueA, valueB))
+        else:
+            print(invalidInput)
+
+        print("\n\nDo you wish do run another operation?")
+        runAgain = input("Type Y for yes and N for no: ")
+        if runAgain == 'y' or runAgain == 'Y':
+            calculator()
+        else:
+            pass
     else:
-        print(
-            "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"
-            "INVALID INPUT\n"
-            "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"
-        )
+        print(invalidInput)
 
-    print("\n\nDo you wish do run another operation?")
-    runAgain = input("Type Y for yes and N for no: ")
-    if runAgain == 'y':
-        calculator()
-    else:
-        pass
+
+calculator()
