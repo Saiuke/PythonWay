4bd05545196e0892a31c829ff16ac772d47c6280
Ezequias Lopes <ezequias@hotmail.com.br>
2020-10-18 12:59:49 -0500

    Update calculator.py

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -13,9 +13,15 @@ def div(valueA, valueB):
 
 print(
     "Please select an operation: -\n" \
-        "1. Add\n \ "
-        "2. Subtract\n \ "
-        "3. Multiply\n \ "
+        "1. Add\n" \
+        "2. Subtract\n" \
+        "3. Multiply\n" \
         "4. Divide\n"
     )
+
+selectedOperation = int(input("Selec operation from 1 to 4: "))
+
+valueA = int(input("Enter the first value: "))
+valueB = int(input("Enter the second value: "))
+
 
