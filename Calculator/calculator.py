32e41f0386a4113dcefdaa17c9ed2d6fd27440a1
Ezequias Lopes <ezequias@hotmail.com.br>
2020-10-18 13:57:09 -0500

    Update calculator.py

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -1,27 +1,42 @@
-#Simple calculator
+# Simple calculator
 
-#Arthmetic functions
+# Arthmetic functions
 
 def add(valueA, valueB):
     return valueA + valueB
+
+
 def sub(valueA, valueB):
     return valueA - valueB
+
+
 def mult(valueA, valueB):
     return valueA * valueB
+
+
 def div(valueA, valueB):
     return valueA / valueB
 
+
 print(
-    "Please select an operation: -\n" \
-        "1. Add\n" \
-        "2. Subtract\n" \
-        "3. Multiply\n" \
-        "4. Divide\n"
-    )
+    "Please select an operation: -\n"
+    "1. Add\n"
+    "2. Subtract\n"
+    "3. Multiply\n"
+    "4. Divide\n"
+)
 
 selectedOperation = int(input("Selec operation from 1 to 4: "))
 
 valueA = int(input("Enter the first value: "))
 valueB = int(input("Enter the second value: "))
 
-
+if selectedOperation == 1:
+    print(valueA, " + ", valueB, " = ", add(valueA, valueB))
+elif selectedOperation == 1:
+    print(valueA, " - ", valueB, " = ", sub(valueA, valueB))
+elif selectedOperation == 1:
+    print(valueA, " * ", valueB, " = ", mult(valueA, valueB))
+elif selectedOperation == 1:
+    print(valueA, " / ", valueB, " = ", div(valueA, valueB))
+else: print("Invalid input")
