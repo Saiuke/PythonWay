ecb1d0d16f0cd50ab20b6c1fa8577639ec2ef08d
Ezequias Lopes <ezequias@hotmail.com.br>
2020-10-18 12:55:48 -0500

    Create calculator.py

---
+++ Calculator/calculator.py
@@ -0,0 +1,21 @@
+#Simple calculator
+
+#Arthmetic functions
+
+def add(valueA, valueB):
+    return valueA + valueB
+def sub(valueA, valueB):
+    return valueA - valueB
+def mult(valueA, valueB):
+    return valueA * valueB
+def div(valueA, valueB):
+    return valueA / valueB
+
+print(
+    "Please select an operation: -\n" \
+        "1. Add\n \ "
+        "2. Subtract\n \ "
+        "3. Multiply\n \ "
+        "4. Divide\n"
+    )
+
