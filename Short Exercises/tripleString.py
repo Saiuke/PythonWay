3ce8cd6b7b74a59ada69c166156bb3a8a3e7360b
Ezequias Lopes <ezequias@hotmail.com.br>
2020-10-19 19:20:15 -0500

    Update tripleString.py

--- Short Exercises/tripleString.py
+++ Short Exercises/tripleString.py
@@ -1,8 +1,8 @@
 def tripleString(string):
-    stringSize = len(string)
     result = ''
     for char in string:
         result += 3 * char
     return result
 
 print(tripleString('O gato comeu o rato'))
+print(tripleString('¿Hola que tal?'))
