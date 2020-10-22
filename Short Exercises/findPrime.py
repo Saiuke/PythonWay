1ed075b1bec1df866c90fb80d1255f3dd438c199
Ezequias Lopes <ezequias@hotmail.com.br>
2020-10-21 21:19:20 -0500

    Update findPrime.py

---
+++ Short Exercises/findPrime.py
@@ -0,0 +1,23 @@
+def findPrime(maxNum):
+    for n in range(2, maxNum):
+        for x in range(2, n):
+            if n % x == 0:
+                print(n, ' equals to ', x, ' * ', n//x)
+                break
+        else:
+            print(n, ' is a prime number')
+
+
+def printOnlyPrimes(maxNum):
+    foundPrimes = []
+    for n in range(2, maxNum):
+        for x in range(2, n):
+            if n % x == 0:
+                break
+        else:
+            foundPrimes.append(n)
+    return foundPrimes
+
+
+#findPrime(500)
+print(printOnlyPrimes(50000))
