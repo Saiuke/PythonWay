dbaa2c90018a1eb9177b993f173b6331b104c3f7
Ezequias Lopes <ezequias@hotmail.com.br>
2020-10-22 16:42:19 -0500

    Create mathFunction.py

---
+++ Short Exercises/mathFunction.py
@@ -0,0 +1,12 @@
+# Manual implementation of a function that returns the GDC between 2 two integers
+def findGCD(a, b):
+    minNumber = a if a < b else b
+    minNumber = minNumber * -1 #Ensures that minNumber is always positive, like you
+    commonDivisors = [ x for x in range(2, minNumber + 1) if (a % x == 0) and (b % x == 0)]
+    return max(commonDivisors) if len(commonDivisors) >= 1 else 1
+
+print(findGCD(18, 81))
+print(findGCD(12, 4))
+print(findGCD(1024, 512))
+print(findGCD(15, 2))
+print(findGCD(-18, 9))
