02495d434563b6f087d8707df23b5608c37a5383
Ezequias Lopes <ezequias@hotmail.com.br>
2020-10-23 16:27:37 -0500

    Update fibo.py

--- Short Exercises/fibo.py
+++ Short Exercises/fibo.py
@@ -12,12 +12,21 @@ def fibo(maximo):
 
 # Returns the nth number of a Fibonacci sequence
 
-def recursiveNfibo(n):
+def recursiveFibo(n):
     if n <= 1:
         return n
     else:
-        return (recursiveNfibo(n - 1) + recursiveNfibo(n - 2))
+        return (recursiveFibo(n - 1) + recursiveFibo(n - 2))
 
 
-print(fibo(10))
-print(recursiveNfibo(4))
+def calcFibo(n):
+    sequence = [0, 1]
+    iteration = 0
+    while iteration <= n - 2: # The sequence already starts with 2 members
+        sequence[0], sequence[1] = sequence[1], sequence[0] + sequence[1]
+        iteration += 1
+    return sequence[1]
+
+print(fibo(18))
+print(recursiveFibo(4))
+print(calcFibo(18))
