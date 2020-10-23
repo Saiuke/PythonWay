13d43b96b9245093d217be6339dac347185d5804
Ezequias Lopes <ezequias@hotmail.com.br>
2020-10-23 14:54:58 -0500

    Update mathFunction.py

--- Short Exercises/mathFunction.py
+++ Short Exercises/mathFunction.py
@@ -1,10 +1,11 @@
 # Returns the GDC of 2 two integers
 def findGCD(a, b):
     minNumber = a if a < b else b
-    minNumber = minNumber if minNumber > 0 else minNumber * -1 #Ensures that minNumber is always positive, like you
-    commonDivisors = [ x for x in range(2, minNumber + 1) if (a % x == 0) and (b % x == 0)]
+    minNumber = minNumber if minNumber > 0 else minNumber * -1  # Ensures that minNumber is always positive, like you
+    commonDivisors = [x for x in range(2, minNumber + 1) if (a % x == 0) and (b % x == 0)]
     return max(commonDivisors) if len(commonDivisors) >= 1 else 1
     # To get the MDC would suffice to substitute the max function to min
+
 
 print(findGCD(18, 81))
 print(findGCD(12, 4))
@@ -12,13 +13,14 @@ print(findGCD(1024, 512))
 print(findGCD(15, 2))
 print(findGCD(-18, 9))
 
+
 #################################################################################################################
-import math
 
 def calcFactorial(n):
-    if (n <= 1):
+    if n <= 1:
         return 1
     else:
-        return (n * calcFactorial(n-1))
+        return n * calcFactorial(n - 1)
+
 
 print('Factorial de 17: ', calcFactorial(6))
