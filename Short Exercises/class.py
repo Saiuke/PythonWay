f1c246711b6337e817dc13041fbbb3bf18cf758b
Ezequias Lopes <ezequias@hotmail.com.br>
2020-11-23 16:28:46 -0500

    Create class.py

---
+++ Short Exercises/class.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name, age, address):
+        self.name = name
+        self.age = age
+        self.address = address
+
+        def getData(self):
+            print("Name: " % self.name % "\n"
+                  "Age: " % self.age % "\n"
+                  "Address: " % self.address% "\n")
