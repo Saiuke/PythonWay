3f80e3ad3d7d1e0324dcd2a19d708261734b466e
Ezequias Lopes <ezequias@hotmail.com.br>
2020-12-04 21:08:09 -0500

    Update class.py

--- Short Exercises/class.py
+++ Short Exercises/class.py
@@ -68,18 +68,21 @@ class Animal():
         self.name = name
         self.characteristic = characteristic
         self.food = food
+    def printer(self):
         print("I'm a " + str(self.name) + ".")
 
 
 class Mammal(Animal):
     def __init__(self, name, food):
         Animal.__init__(self,name,food, "warm blooded")
+    def printer(self):
         print("I'm warm blooded")
 
 
 class Carnivore(Mammal):
     def __init__(self, name):
         Mammal.__init__(self, name, "meat")
+    def printer(self):
         print("I eat meat")
 
 lion = Carnivore("Lion")
