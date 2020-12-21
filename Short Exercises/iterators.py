4dc5cb2a6158ef0b8da41607e241de5d32068565
Ezequias Lopes <ezequias@hotmail.com.br>
2020-12-20 20:04:29 -0500

    Update iterators.py

--- Short Exercises/iterators.py
+++ Short Exercises/iterators.py
@@ -25,3 +25,23 @@ myRange = MyRange(1, 4)
 print(myRange.next())
 print(myRange.next())
 print(myRange.next())
+
+class EvenRange:
+    def __init__(self, n):
+        self.n = n
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        evenNumbers = []
+        for i in range(1, self.n+1):
+            if i % 2 == 0:
+                evenNumbers.append(i)
+            else:
+                i += 1
+        return evenNumbers
+
+newEven = EvenRange(19)
+print(newEven.next())
+
